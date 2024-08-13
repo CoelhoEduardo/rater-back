ab966efe82b3f7446af68c0da295a50a35c41f26
Eduardo Coelho <educs98@outlook.com>
2024-08-13 11:39:54 -0300

    implement user route

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,11 @@
 import Fastify from "fastify";
+import userRoutes from "./modules/user/user.route";
 
 const server = Fastify();
 
-server.get("/helloworld", async (req, res) => {
-  return { message: "Hello World" };
-});
-
 async function main() {
+  server.register(userRoutes, { prefix: "api/users" });
+
   try {
     await server.listen({ port: 3000, host: "0.0.0.0" });
     console.log("Server listening at http://localhost:3000");
